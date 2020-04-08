3457880d0ab3ae0fac0a0beaf9eba388df3d73cd
liuyuanjie <yjl5062@hotmail.com>
2020-04-08 16:06:12 +0800

    初始化项目

---
+++ utils/util.js
@@ -0,0 +1,24 @@
+const request=url=>{
+  wx:showLoading({
+    titlw:'加载中'
+  })
+  return new Promise((resolve,reject)=>{
+    wx.request({
+      url:url,
+      success(res){
+        wx.hideLoading()
+        resolve(res)
+      },
+      fail(res){
+        wx.hideLoading()
+        reject(res)
+      },
+      complete(){
+        wx.hideLoading()
+      }
+    })
+  })
+}
+module.exports = {
+  request
+}
