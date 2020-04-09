0b5a7453a67ad6891c1d7e31bd51cdf6ad37b3a3
liuyuanjie <yjl5062@hotmail.com>
2020-04-09 10:12:06 +0800

    优选列表完成

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,29 +1,41 @@
-import util from '../../utils/util'
-import servicePath from './../../utils/config';
+import util from "../../utils/util";
+import servicePath from "./../../utils/config";
 //获取应用实例
-const app = getApp()
+const app = getApp();
 
 Page({
   data: {
-   goods:[]
+    goods: [],
   },
   onLoad: function () {
-      util.request(servicePath.goodsPage).then(
-        res=>{
-          console.log(res)
-          this.setData({
-            goods:res.data.goods
-          })
-        }
-      )
+    util.request(servicePath.goodsPage).then((res) => {
+      console.log(res);
+      this.setData({
+        goods: res.data.goods,
+      });
+    });
   },
-  onShareAppMessage(){
+  onPullDownRefresh() {
+    wx.showToast({
+      title: "刷新完成",
+      icon: "success",
+      duration: 1500,
+    });
+  },
+  onReachBottom() {
+    wx.showToast({
+      title: "人家可是有底线的",
+      icon: "none",
+      duration: 1000,
+    });
+  },
+  onShareAppMessage() {
     return {
-      title:'什么值得买京东优选',
-      path:'pages/index/index',
-      success:function () {
-        console.log('success')
-      }
-    }
-  }
-})
+      title: "什么值得买京东优选",
+      path: "pages/index/index",
+      success: function () {
+        console.log("success");
+      },
+    };
+  },
+});
